b65cae3caa6773415ac5c9c42795f379f92c5dea
zswitten <zswitten@gmail.com>
2020-01-23 19:34:56 -0800

    convs in right place

--- adapt_model.py
+++ adapt_model.py
@@ -4,6 +4,7 @@ from keras.layers.recurrent import SimpleRNN
 from keras.layers import Dense, Activation, Bidirectional, Flatten, Lambda, Input, Embedding
 from keras.optimizers import SGD, adam
 from keras.layers import ZeroPadding1D, Convolution1D, Convolution2D, GlobalMaxPooling1D
+from keras.layers import Conv1D
 from keras.layers import TimeDistributed, Dropout, GaussianNoise
 from keras.layers.merge import add, concatenate
 from keras.activations import relu
@@ -18,18 +19,18 @@ def adapt_model(kds_model, input_dim=26, max_query_len=30, embedding_dim=32, max
     fc3 = kds_model.get_layer('time_distributed_3')(fc2)
     birnn = kds_model.get_layer('bidirectional_1')(fc3)
     char_preds = kds_model.get_layer('out')(birnn)  # Maybe should be get_layer('ypred')?
-    pool = Flatten()(char_preds)
+    conv1 = Conv1D(filters=256, kernel_size=7, padding='same', activation='relu')(char_preds)
+    max_pool1 = GlobalMaxPooling1D()(conv1)
     # ADD NOISE
     # pool = GaussianNoise(100)(pool)
 
     query = Input(shape=(max_query_len,), name='query')
     query_embed = Embedding(input_dim=len(char_map), output_dim=embedding_dim, input_length=max_query_len)(query)
-    query_encode = Flatten()(query_embed)
+    conv2 = Conv1D(filters=256, kernel_size=7, padding='same', activation='relu')(query_embed)
+    max_pool2 = GlobalMaxPooling1D()(conv2)
 
-    merged = concatenate([query_encode, pool])
-    conv1 = Conv1D(filters=256, kernel_size=7, padding='same', activation='relu')(merged)
-    max_pool1 = GlobalMaxPooling1D()(conv1)
-    linear_regression = Dense(256)(max_pool1)
+    merged = concatenate([max_pool1, max_pool2])
+    linear_regression = Dense(256)(merged)
     y_pred = Dense(1, name="y_pred", activation="sigmoid")(linear_regression)
     model = Model(inputs=[input_data, query], outputs=y_pred)
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
